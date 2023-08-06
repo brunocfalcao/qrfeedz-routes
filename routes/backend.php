d8470a014b01da010bdb258e01f00cdf3bc127b0
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-08-06 17:03:20 +0200

    wip

--- routes/backend.php
+++ routes/backend.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'welcome to the backend.';
 });
+
+//Route::get('/password/reset/{token}', [PagesController::class, 'layoutsSignUp1'])->name('layouts/sign-up-1');
--- routes/testing.php
+++ routes/testing.php
@@ -1,1 +1,7 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/tests/{view}', function ($view) {
+    return view("qrfeedz-backend::tests/{$view}");
+});
