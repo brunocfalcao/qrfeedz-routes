5e7b64f4581ffa98fb2bc8ade71e51679c31968d
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-08-09 00:18:08 +0200

    Update backend.php

--- routes/backend.php
+++ routes/backend.php
@@ -2,14 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'welcome to the backend.';
-});
-
 Route::middleware('auth:backend')->group(function () {
-    Route::get('home', function () {
-        return 'welcome to the backend home';
-    })->name('backend.home');
+    Route::view('/', 'qrfeedz-backend::backend.home')
+         ->name('backend.home');
 });
 
 //Route::get('/login', [\App\Http\Controllers\AuthController::class, 'loginView'])->name('loginView');
