eb01ed70466c2729de40338521032dacf39e8f47
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-08-17 01:25:07 +0200

    Update frontend.php

--- routes/frontend.php
+++ routes/frontend.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use QRFeedz\Frontend\Controllers\QRCodeController;
 
 Route::get('/', function () {
-    return 'welcome to the frontend.';
+    return 'welcome to the public website qrfeedz.';
 });
+
+Route::get(
+    'qrcode/{uuid}', // This is the questionnaire uuid.
+    [QRCodeController::class, 'go']
+)->name('qrcode.go');
+
+Route::get(
+    'qrcode/pages/{uuid}', // This is the page instance uuid.
+    [QRCodeController::class, 'renderPageInstance']
+)->name('qrcode.render-page-instance');
